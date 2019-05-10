797dec27c955fa7fd558096fb5d2ea185a8d5924
Swapnil Gupta <32034605+swapnilkumargupta@users.noreply.github.com>
2019-05-10 08:57:29 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import textblob
 
 
-consumer_key='iA8XtD33RyfySXETKfFROVqvB'
-consumar_secret='sD4e0oISB2XEBpEQWtt5oJRxDYtbRQWPj4vaPW9VUmOWxKjfFv '
+consumer_key='C_K'
+consumar_secret='C_S'
 
-access_token ='382441070-gMoVwamM2RaM6MWL0io3FWRPPM5hJZxHkgePYfs3 '
-access_token_secret='GAsrYUfTuKkHZH25CIg35JQwYgYvefzazBdeeQ7x81dku'
+access_token ='A_T'
+access_token_secret='_T_S'
 
 auth=tweepy.OAuthHandler(consumer_key,consumar_secret)
 auth.set_access_token(access_token,access_token_secret)
